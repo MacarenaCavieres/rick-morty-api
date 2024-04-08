add457e9564c82d5cc738f3d0ed7704e640a7545
MCavieres <maca.c.91@gmail.com>
2024-04-08 18:05:23 -0400

    se imprimen las tarjetas en el dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 const contenido = document.querySelector("#contenido");
 const template = document.querySelector("#template");
-const fragment = document.createDocumentFragment;
+const fragment = document.createDocumentFragment();
 
 const fetchApi = async () => {
     try {
@@ -16,7 +16,20 @@ const pintarTarjeta = (data) => {
     console.log(data.results);
     const datos = data.results;
 
-    datos.forEach((item) => {});
+    contenido.textContent = "";
+    datos.forEach((item) => {
+        const clone = template.content.firstElementChild.cloneNode(true);
+        clone.querySelector(".card-title").textContent = item.name;
+        clone.querySelector(".status").textContent = `${item.species} - ${item.status}`;
+        clone.querySelector(".origin").textContent = `Origin: ${item.origin.name}`;
+        clone.querySelector(".location").textContent = `Location: ${item.location.name}`;
+        clone.querySelector(".gender").textContent = `Gender: ${item.gender}`;
+        clone.querySelector(".img-fluid").src = item.image;
+        clone.querySelector(".img-fluid").alt = item.name;
+
+        fragment.appendChild(clone);
+    });
+    contenido.appendChild(fragment);
 };
 
 fetchApi();
