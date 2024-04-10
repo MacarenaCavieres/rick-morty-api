4dff4070d4262571be760db85d41e3240372a078
MCavieres <maca.c.91@gmail.com>
2024-04-09 22:09:52 -0400

    se implementan los filtros

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,6 @@ const contenido = document.querySelector("#contenido");
 const template = document.querySelector("#template");
 const previous = document.querySelector("#previous");
 const next = document.querySelector("#next");
-// const human = document.querySelector("#human");
-// const alien = document.querySelector("#alien");
-// const others = document.querySelector("#others");
-// const alive = document.querySelector("#alive");
-// const dead = document.querySelector("#dead");
-// const unknown = document.querySelector("#unknown");
 const fragment = document.createDocumentFragment();
 
 let page = 1;
@@ -49,6 +43,7 @@ const pintarTarjeta = (data) => {
         clone.querySelector(".origin").textContent = `Origin: ${item.origin.name}`;
         clone.querySelector(".location").textContent = `Location: ${item.location.name}`;
         clone.querySelector(".gender").textContent = `Gender: ${item.gender}`;
+        clone.querySelector(".gender").classList.add(`${item.gender}gender`);
         clone.querySelector(".img-fluid").src = item.image;
         clone.querySelector(".img-fluid").alt = item.name;
 
@@ -66,16 +61,25 @@ filters.addEventListener("click", (e) => {
     const filter = e.target.value.toLowerCase().trim();
 
     tarjetas.forEach((item) => {
-        const status = item.querySelector(".status");
-        const specie = status.textContent.split("-")[0].toLowerCase().trim();
+        const general = item.querySelector(".status");
+        const specie = general.textContent.split("-")[0].toLowerCase().trim();
+        const status = general.textContent.split("-")[1].toLowerCase().trim();
+        const gender = item.querySelector(".gender").textContent.split(":")[1].toLowerCase().trim();
+        const genderUnknown = item.querySelector(".unknowngender");
+
+        console.log(filter);
+        console.log(genderUnknown);
+
         if (e.target.checked && filter === specie) {
+            item.style.display = "flex";
+        } else if (e.target.checked && filter === status) {
+            item.style.display = "flex";
+        } else if (e.target.checked && filter === gender) {
+            item.style.display = "flex";
+        } else if (!e.target.checked) {
             item.style.display = "flex";
         } else {
             item.style.display = "none";
-        }
-
-        if (e.target.checked && filter === "all") {
-            item.style.display = "flex";
         }
     });
 });
